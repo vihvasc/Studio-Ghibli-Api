67247be395c550b97e4de04909bb6500478733f3
Virginia <vihvasc@gmail.com>
2022-10-13 16:25:54 -0300

    feat: component NavBar

---
+++ studio-ghibli/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/navbar.css';
+
+function Navbar() {
+    return (
+        <div className='nav-img'>
+            <img src='https://www.pngkey.com/png/full/95-958842_-dvd-anime-studio-ghibli-collector-edition-24.png'></img>
+        </div>
+    )
+}
+
+export default Navbar;
